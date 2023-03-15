3e6a5d9cc2e9de33671853e4debad3c3b8944d85
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-03-15 17:15:55 -0400

    this was rough

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    fibonacci_seq = []
+    if length > 0:
+        fibonacci_seq.append(0)
+        if length > 1:
+            fibonacci_seq.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_seq.append(
+                        fibonacci_seq[i-1] + fibonacci_seq[i-2])
+
+    print(fibonacci_seq)
+    
